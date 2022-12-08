0aab057feba1e23e7b9e68ce4d7f19c615de9641
thisyujeong <this.yujeong@gmail.com>
2022-12-08 15:38:22 +0900

    install clean-webpack-plugin and setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js', // start file
@@ -28,6 +29,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'common.css',
     }),
+    new CleanWebpackPlugin(),
   ],
   devServer: {
     static: {
