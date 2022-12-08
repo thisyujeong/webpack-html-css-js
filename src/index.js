42d461a947cb5095e699e9f71805adf135eb2510
thisyujeong <this.yujeong@gmail.com>
2022-12-08 15:32:09 +0900

    css, image loader setting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { hello } from './util';
+import './style.css';
+import './header.css';
+import logo from './images/image.png';
 
-const text = hello('hello webpack');
+const text = hello('<h1>hello webpack</h1>');
+const img = `<img src="${logo}" alg="apple wallpaper" width="500"/>`;
 
-document.getElementById('root').innerHTML = text;
+document.getElementById('root').innerHTML = text + img;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js', // start file
@@ -7,9 +8,25 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.png$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'common.css',
     }),
   ],
   devServer: {
